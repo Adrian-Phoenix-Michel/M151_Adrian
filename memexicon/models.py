6da373c7b9b8037ff65aaf63b7eb2964260e90a2
vmadmin <adrian.p.michel@hotmail.com>
2020-01-29 09:42:27 +0100

    Version 0.1

---
+++ memexicon/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Memes(models.Model):
+    meme_img = models.ImageField()
+    meme_name = models.CharField(max_length=50)
+    popularity = models.IntegerField()
+
+class MemesInDetail(models.Model):
+    meme_id = models.ForeignKey(Memes, on_delete=models.CASCADE)
+    img_fk = models.ForeignKey(Memes.meme_img, on_delete=models.CASCADE)
+    name_fk = models.ForeignKey(Memes.meme_name, on_delete=models.CASCADE)
+    popularity_fk = models.ForeignKey(Memes.popularity, on_delete=models.CASCADE)
+    origin = models.CharField(max_length=1000)
