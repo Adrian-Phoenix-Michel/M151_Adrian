bd1d8eced1880bfa84f41c70b1ca94568a15c11b
vmadmin <adrian.p.michel@hotmail.com>
2020-01-29 10:39:55 +0100

    Version 0.1

--- M151_Adrian/settings.py
+++ M151_Adrian/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'M151_Adrian.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Metro2034Redux',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
