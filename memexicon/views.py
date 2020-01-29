28dad0750d84c91f548f77868f5b265edc14d952
vmadmin <adrian.p.michel@hotmail.com>
2020-01-29 11:53:48 +0100

    Version 0.1

--- memexicon/views.py
+++ memexicon/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from memexicon.forms import MemesForm
+
 
 # Create your views here.
+
+def memexicon_view(request):
+    if request.method == "POST":
+        form = MemesForm(request.POST)
+        if form.is_valid():
+            memexicon = form.save(commit=False)
+            memexicon.save()
+    else:
+        form = MemesForm()
+    return render(request, 'memexicon.html', {'form': form})
