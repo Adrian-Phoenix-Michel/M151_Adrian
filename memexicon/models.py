9746b53604f601200e7b69d3128458c2b063d474
vmadmin <adrian.p.michel@hotmail.com>
2020-01-29 11:26:06 +0100

    Version 0.1

--- memexicon/models.py
+++ memexicon/models.py
@@ -11,7 +11,4 @@ class Memes(models.Model):
 
 class MemesInDetail(models.Model):
     meme_id = models.ForeignKey(Memes, on_delete=models.CASCADE)
-    img_fk = models.ForeignKey(Memes.meme_img, on_delete=models.CASCADE)
-    name_fk = models.ForeignKey(Memes.meme_name, on_delete=models.CASCADE)
-    popularity_fk = models.ForeignKey(Memes.popularity, on_delete=models.CASCADE)
     origin = models.CharField(max_length=1000)
