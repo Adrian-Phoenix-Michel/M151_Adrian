31c0ee1f8247d1a18876614c81f36af357cf9061
vmadmin <adrian.p.michel@hotmail.com>
2020-01-31 09:48:33 +0100

    Version 0.1_5

--- M151_Adrian/settings.py
+++ M151_Adrian/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'M151_Adrian.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'Metro2034Redux',
